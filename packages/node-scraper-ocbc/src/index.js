1f36f123b8be06676586e90a95b4b7bdc2f18338
jhtong <me@joeltong.org>
2019-08-27 09:36:40 +0800

    added addresses

--- packages/node-scraper-ocbc/src/index.js
+++ packages/node-scraper-ocbc/src/index.js
@@ -6,6 +6,7 @@ import scrapeOffers from './lib/fetcher';
   const results = await scrapeOffers();
   // console.dir(results);
   console.log(util.inspect(results, false, null, true /* enable colors */));
+  // results.map(r => console.log(JSON.stringify(r)));
   console.log(results.length);
 })();
 
